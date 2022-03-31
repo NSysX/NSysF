c73574b60f4aa76698dbd24e275ba7dc7db188c3
Eduardo Navarro <eduardo_na@hotmail.com>
2022-03-31 16:13:18 -0500

    Creacion de Proyecto

---
+++ NSysF/src/NSysF.Application/Application.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSysF.Application
+{
+    internal class Application
+    {
+    }
+}
